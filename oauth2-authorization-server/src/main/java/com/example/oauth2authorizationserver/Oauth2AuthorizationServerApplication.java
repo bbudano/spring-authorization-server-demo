1585dd21ec34b00946095018904421015728f519
Bernard Budano <bbudano@gmail.com>
2025-02-19 16:04:42 +0100

    Users and authorities persisted to db

--- oauth2-authorization-server/src/main/java/com/example/oauth2authorizationserver/Oauth2AuthorizationServerApplication.java
+++ oauth2-authorization-server/src/main/java/com/example/oauth2authorizationserver/Oauth2AuthorizationServerApplication.java
@@ -1,11 +1,16 @@
 package com.example.oauth2authorizationserver;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Oauth2AuthorizationServerApplication {
@@ -15,15 +20,27 @@ public class Oauth2AuthorizationServerApplication {
 	}
 
 	@Bean
-	UserDetailsService userDetailsService() {
-		var user = User
-				.withUsername("bernard")
-				// password is pw
-				.password("{noop}pw")
-				.roles("user")
-				.build();
+	PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
 
-		return new InMemoryUserDetailsManager(user);
+	@Bean
+	JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
+	}
+
+	@Bean
+	ApplicationRunner applicationRunner(UserDetailsManager userDetailsManager, PasswordEncoder passwordEncoder) {
+		return args -> {
+			if (!userDetailsManager.userExists("bernard")) {
+				var user = User.withUsername("bernard")
+					.password(passwordEncoder.encode("pw"))
+					.roles("user")
+					.build();
+
+				userDetailsManager.createUser(user);
+			}
+		};
 	}
 
 }
