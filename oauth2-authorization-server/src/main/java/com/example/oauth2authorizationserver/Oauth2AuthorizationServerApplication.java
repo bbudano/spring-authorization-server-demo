d03b04c638290df7b65e030a7b9278a2ce61de98
Bernard Budano <bbudano@gmail.com>
2025-02-25 21:55:03 +0100

    rsa key pair stored in hashicorp vault

--- oauth2-authorization-server/src/main/java/com/example/oauth2authorizationserver/Oauth2AuthorizationServerApplication.java
+++ oauth2-authorization-server/src/main/java/com/example/oauth2authorizationserver/Oauth2AuthorizationServerApplication.java
@@ -2,8 +2,10 @@ package com.example.oauth2authorizationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RSAKeyProperties.class)
 public class Oauth2AuthorizationServerApplication {
 
 	public static void main(String[] args) {
